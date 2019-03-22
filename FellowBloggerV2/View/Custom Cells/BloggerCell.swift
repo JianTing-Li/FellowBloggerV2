49a2203a6cbf67c6f0cf2318f7bc41120603d5f1
Jian Ting Li <jiantingli@Jian-Tings-MacBook-Pro.local>
2019-03-22 11:46:14 -0400

    user is able search blogger by full name or username

---
+++ FellowBloggerV2/View/Custom Cells/BloggerCell.swift
@@ -0,0 +1,17 @@
+//
+//  BloggerCell.swift
+//  FellowBloggerV2
+//
+//  Created by Jian Ting Li on 3/22/19.
+//  Copyright © 2019 Jian Ting Li. All rights reserved.
+//
+
+import UIKit
+
+class BloggerCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: CircularImageView!
+    @IBOutlet weak var userFullNameLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+}
